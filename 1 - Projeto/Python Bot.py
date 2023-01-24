38d8ef03b7032ecef9a31951dfe409e27181faf1
Glfx20 <jsgleisson@gmail.com>
2023-01-24 17:16:26 -0300

    commit

--- 1 - Projeto/Python Bot.py
+++ 1 - Projeto/Python Bot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,7 +9,11 @@ import pandas as pd
 import time
 
 options = Options()
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-logging")
+options.add_argument("--log-level=3")
 options.add_argument("start-maximized")
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
 link = ("https://pbdatatrader.com.br/jogosdodia")
